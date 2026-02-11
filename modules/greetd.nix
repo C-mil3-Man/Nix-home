49131d906883c8b035c64f9c958e1f973a48e3a9
cmil3man <kalevakotoa@gmail.com>
2026-02-12 00:41:21 +0100

    test regreet2

--- modules/greetd.nix
+++ modules/greetd.nix
@@ -1,10 +1,20 @@
 {pkgs, ...}: {
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        command = "${pkgs.cage}/bin/cage -s -- ${pkgs.greetd.regreet}/bin/regreet";
+        user = "greeter";
+      };
+    };
+  };
+
   programs.regreet = {
     enable = true;
     settings = {
       background = {
-        path = "/home/crux/Pictures/wallpapers/lainwp.jpg";
-        fit = "Cover"; # or "Fill", "Contain", "ScaleDown"
+        path = "/usr/share/wallpapers/lainwp.jpg";
+        fit = "Cover";
       };
       GTK = {
         application_prefer_dark_theme = true;
@@ -12,15 +22,8 @@
     };
   };
 
-  services.greetd = {
-    enable = true;
-    settings = {
-      default_session = {
-        command = "${pkgs.regreet}/bin/regreet";
-        user = "greeter";
-      };
-    };
-  };
+  # Ensure greeter user has proper environment
+  environment.systemPackages = with pkgs; [cage];
 
-  security.pam.services.greetd = {};
+  security.pam.services.greetd.enableGnomeKeyring = true;
 }
